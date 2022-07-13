0848e6154d7f0515681eb868f0d41bb17f1b4b25
Matteo VG7 <57224795+matteovg7@users.noreply.github.com>
2022-07-13 10:53:57 +0200

    fix image

--- src/Http/Controllers/Admin/MediaLibraryController.php
+++ src/Http/Controllers/Admin/MediaLibraryController.php
@@ -209,7 +209,7 @@ class MediaLibraryController extends ModuleController implements SignUploadListe
         }
 
         $fields = [
-            'uuid' => $request->input('key') ?? $request->input('blob'),
+            'uuid' => $request->input('key') ? $path[count($path) - 2] . "/" . $path[count($path) - 1] : $request->input('blob'),
             'filename' => $request->input('name'),
             'width' => $request->input('width'),
             'height' => $request->input('height'),
